0ee1f571ad7bf7e9f78202a7c2278ca075d6ca78
Vladimir Kuchin <126107125+vladimirkucin634@users.noreply.github.com>
2025-05-02 02:56:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsonl_tools',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python library for working with JSONL files',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
