a41594e77a08e22d5ab19ad1df93b4b3680975f8
Vladimir Kuchin <126107125+vladimirkucin634@users.noreply.github.com>
2025-05-04 01:57:29 +0300

    Update __init__.py

--- jsonl_tools/__init__.py
+++ jsonl_tools/__init__.py
@@ -1,7 +1,15 @@
 from .core import load, loads, dump, dumps
-from .convert import json_to_jsonl, jsonl_to_json
+from .convert import (
+    json_to_jsonl,
+    jsonl_to_json,
+    jsonl_to_csv,
+    csv_to_jsonl
+)
+from .process import find, transform, remove_key
 
 __all__ = [
     "load", "loads", "dump", "dumps",
-    "json_to_jsonl", "jsonl_to_json"
+    "json_to_jsonl", "jsonl_to_json",
+    "jsonl_to_csv", "csv_to_jsonl",
+    "find", "transform", "remove_key"
 ]
